812c13728ad29a2ecc72c9ca4049861d76177dbf
elrobertrocha91 <robertoestebanrocha91@gmail.com>
2023-07-24 19:09:27 -0300

    new components

---
+++ Frontend/src/components/CoursesList/CoursesList.jsx
@@ -0,0 +1,10 @@
+import { useGetFacultyQuery } from "../../features/facultyApiSlice";
+
+function CoursesList () {
+
+    return(
+        <div>CoursesList</div>
+    )
+}
+
+export default CoursesList;
---
+++ Frontend/src/components/TeacherList/TeacherList.jsx
@@ -0,0 +1,10 @@
+import { useGetTeacherQuery } from "../../features/teacherApiSlice";
+
+function TeacherList () {
+
+    return(
+        <div>TeacherList</div>
+    )
+}
+
+export default TeacherList;
