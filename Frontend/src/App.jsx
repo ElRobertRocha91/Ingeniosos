33beb2e55aeaf4e25b5d078a554903a39e97b316
elrobertrocha91 <robertoestebanrocha91@gmail.com>
2023-07-26 22:41:26 -0300

    changes in App

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,14 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Home from '../src/pages/Home';
+import './App.css';
 
 function App() {
 
 
   return (
     <>
-      <div>Hello Word</div>
+      <div>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+        </Routes>
+      </div>
     </>
   )
 }
