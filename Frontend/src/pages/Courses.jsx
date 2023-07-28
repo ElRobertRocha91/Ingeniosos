b001457a8cad7022c043cc31aed150cb969361e5
elrobertrocha91 <robertoestebanrocha91@gmail.com>
2023-07-27 22:43:22 -0300

    adding pages

---
+++ Frontend/src/pages/Courses.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Courses () {
+
+    return (
+        <div>Information of Courses</div>
+    )
+}
+
+export default Courses;
---
+++ Frontend/src/pages/Home.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Home () {
+
+    return (
+        <div>Home principal Ingeniosos</div>
+    )
+}
+
+export default Home;
