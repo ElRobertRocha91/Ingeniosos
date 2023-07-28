7a904ef2d885c2d21a89d25c001668e4075c4f77
elrobertrocha91 <robertoestebanrocha91@gmail.com>
2023-07-27 22:55:25 -0300

    test demo TeacherList

--- Frontend/src/components/TeacherList/TeacherList.jsx
+++ Frontend/src/components/TeacherList/TeacherList.jsx
@@ -2,6 +2,13 @@ import { useGetTeacherQuery } from "../../features/teacherApiSlice";
 
 function TeacherList () {
 
+    const { data, isError, isLoading, error } = useGetTeacherQuery();
+
+    if(isLoading) return (<div>Loading...</div>);
+    else if(isError) return (<div>Error: {error.message}</div>)
+
+    console.log(data);
+
     return(
         <div>TeacherList</div>
     )
