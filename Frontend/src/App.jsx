89c8cd19b659036926dbbd914b49e2922dbae0a5
elrobertrocha91 <robertoestebanrocha91@gmail.com>
2023-07-28 23:57:02 -0300

    new changes

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Home from '../src/pages/Home';
+import About from '../src/pages/About';
+import Courses from '../src/pages/Courses';
+import Location from '../src/pages/Location';
+import Error from '../src/pages/Error';
 import './App.css';
 
 function App() {
@@ -11,6 +15,10 @@ function App() {
       <div>
         <Routes>
           <Route path='/' element={<Home/>}/>
+          <Route path='/about' element={<About/>}/>
+          <Route path='/courses' element={<Courses/>}/>
+          <Route path='/location' element={<Location/>}/>
+          <Route path='*' element={<Error/>}/>
         </Routes>
       </div>
     </>
