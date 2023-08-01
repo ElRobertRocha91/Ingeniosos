6b6aa3fb539aa30cc957cc3c959ee146f39904b6
elrobertrocha91 <robertoestebanrocha91@gmail.com>
2023-08-01 15:21:46 -0300

    Adding components the home page

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,9 +1,21 @@
 import React from "react";
+import NavBar from '../components/NavBar/NavBar';
+import Publications from "../components/Publications/Publications";
 
 function Home () {
 
     return (
-        <div>Home principal Ingeniosos</div>
+        <div>
+            <div>
+                <img src="" alt="Image"/>
+                <h2>Centro de Apoyo Escolar</h2>
+                <h2>Ingeniosos</h2>
+                <button>Inscribirse</button>
+                <br/>
+                <NavBar/>
+                <Publications/>
+            </div>
+        </div>
     )
 }
 
